2687483d7e26ca1cccff2d8f3987b2d6ffe16038
“udit01” <uditjainiitd@gmail.com>
2018-01-07 01:45:31 +0530

    added main and help bot dialogues

--- app.js
+++ app.js
@@ -46,21 +46,84 @@ const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' +
 // Main dialog with LUIS
 var recognizer = new builder.LuisRecognizer(LuisModelUrl);
 var intents = new builder.IntentDialog({ recognizers: [recognizer] })
-.matches('Greeting', (session) => {
-    session.send('You reached Greeting intent, you said \'%s\'.', session.message.text);
-})
-.matches('Help', (session) => {
-    session.send('You reached Help intent, you said \'%s\'.', session.message.text);
-})
-.matches('Cancel', (session) => {
-    session.send('You reached Cancel intent, you said \'%s\'.', session.message.text);
-})
+
+// .matches('Greeting', (session) => {
+//     session.send('You reached Greeting intent, you said \'%s\'.', session.message.text);
+// })
+// .matches('Help', (session) => {
+//     session.send('You reached Help intent, you said \'%s\'.', session.message.text);
+// })
+// .matches('Cancel', (session) => {
+//     session.send('You reached Cancel intent, you said \'%s\'.', session.message.text);
+// })
 /*
 .matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
 */
-.onDefault((session) => {
+// .onDefault((session) => {
+//     session.send('Sorry, I did not understand \'%s\'.', session.message.text);
+// });
+
+bot.dialog('/', intents);   
+intents.matches('main', '/main');
+// intents.matches('intro', '/intro');
+intents.matches('Help', '/help');
+intents.matches('Greeting', '/greeting');
+intents.matches('Cancel', '/cancel');
+intents.matches('None', '/none');
+intents.matches('repeat', '/repeat');
+
+// different than campus bot
+intents.onDefault((session) => {
     session.send('Sorry, I did not understand \'%s\'.', session.message.text);
 });
 
-bot.dialog('/', intents);    
 
+var introMessage = ['This is the intro message has a binary string collection',
+    'Used in \help',
+    'Write here the functionalites of our bot',
+    'Like generic and specific search etc'
+];
+
+bot.dialog('/main', [
+    function (session, args, next) {
+        //check for the user-data completeness here
+    },
+    function (session, args, next) {
+        //think on the line
+        builder.Prompts.text(session, 'This is the prompt in main inent');
+    },
+    function (session, results) {
+        if (results.response) {
+            if (results.response.entity === 'Exit') {
+                session.endDialog("You can chat again by saying Hi");
+            }
+            else {
+                session.send('Hey! you are in the main intent!');                
+            }
+        }
+        else {
+            session.endDialog("Invalid Response. You can call again by saying Hi");
+        }
+    },
+    function (session, results) {
+        // The menu runs a loop until the user chooses to (quit).
+        session.replaceDialog('/main');
+    }
+]);
+
+bot.dialog('/help', [
+    function (session) {
+        var introCard = new builder.HeroCard(session)
+            .title("Analyzer Bot")
+            .text("Increasing your productivity")
+            .images([
+                builder.CardImage.create(session, "https://googleen.i.lithium.com/html/assets/Google_Analytics.png")
+            ]);
+        var msg = new builder.Message(session).attachments([introCard]);
+        session.send(msg);
+        introMessage.forEach(function (ms) {
+            session.send(ms);
+        });
+        session.endDialog();
+    }
+]);
