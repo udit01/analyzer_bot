6a428cc0b8ba502d8a64333ac5ad5f0710ff8075
akshat-khare <akshat.khare08@gmail.com>
2018-01-10 21:41:44 +0530

    edited oxfordsearch too

--- src/oxfordSearch.js
+++ src/oxfordSearch.js
@@ -87,19 +87,24 @@ let getOxfordData=function(inp,funcThoughRoot){
                 var reqJson = JSON.parse(body);
 
                 var finjson = JSON.stringify(reqJson, null, '  ');
+                // console.log(finjson);
                 // console.log("Root detected LINE87: "+inp);
                 var lexicalentries = reqJson["results"][0]["lexicalEntries"];
                 var lengthlexicalentries = lexicalentries.length;
-                
+                var oxJSON = {};
+                oxJSON["types"] = [];
                 
                 //
                 for(var i=0;i<lengthlexicalentries;i++){
                     var lexicalcategory = lexicalentries[i]["lexicalCategory"];
-                    findata += "Lexical Category: "  + lexicalcategory+ "\n\n";
+                    oxJSON["types"][i] = {};
+                    oxJSON["types"][i]["lexicalCategory"] = lexicalentries[i]["lexicalCategory"];
+                    //findata += "Lexical Category: "  + lexicalcategory+ "\n\n";
                     // console.log("lexcat was "+i);
                     var entries = lexicalentries[i]["entries"];
                     var numentries = entries.length;
                     var numdef =0;
+                    oxJSON["types"][i]["diffDefinitions"] = [];
                     for(var j=0;j<numentries;j++){
                         var thisentry = entries[j];
                         var sensesarr = thisentry["senses"];
@@ -110,13 +115,16 @@ let getOxfordData=function(inp,funcThoughRoot){
                             try{
                                 var definition = thissense["definitions"][0];
                                 numdef++;
-                                findata += "Definition "+numdef+": "+definition +"\n\n";
+                                oxJSON["types"][i]["diffDefinitions"][numdef-1] = {};
+                                oxJSON["types"][i]["diffDefinitions"][numdef-1]["definition"] =  thissense["definitions"][0];
+                               // findata += "Definition "+numdef+": "+definition +"\n\n";
                                 try{
                                     var examples= thissense["examples"][0]["text"];
-                                    findata += "Example: "+ examples + "\n\n";
+                                    oxJSON["types"][i]["diffDefinitions"][numdef-1]["example"] = thissense["examples"][0]["text"];
+                                    //findata += "Example: "+ examples + "\n\n";
                                 }catch(e){
                                     // console.log("cant find example");
-                                    findata += "\n";
+                                    //findata += "\n";
 
                                 }
                             }catch(e){
@@ -135,7 +143,7 @@ let getOxfordData=function(inp,funcThoughRoot){
                 stringCode = "Coundn't find meaning on API CALL or response was HTML";
             }
 
-            funcThoughRoot(findata,inp,stringCode);
+            funcThoughRoot(oxJSON,inp,stringCode);
             
         });
         response.on('error', function (e) {
@@ -152,7 +160,7 @@ module.exports = {
 }
 
 //made for debugging
-getOxfordData("set",
-    function(enddat){
-        console.log(enddat);
-    }); 
+// get_meaning("set",
+//     function(enddat){
+//         console.log(JSON.stringify(enddat, null, '  '));
+//     }); 
