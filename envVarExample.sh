859c92ad514adcf6e9c9aa6c97a4cb4058000363
“udit01” <uditjainiitd@gmail.com>
2018-01-13 21:52:20 +0530

    2 working and packages saved and app running

---
+++ envVarExample.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+## The following env variables will be taken from your azure bot "Application Settings tab"
+export AzureWebJobsStorage=DefaultEndpointsProtocol=""
+export AzureWebJobsDashboard=DefaultEndpointsProtocol=""
+export WEBSITE_NODE_DEFAULT_VERSION=""
+export BotEnv=""
+export BotId=""
+export MicrosoftAppId=""
+export MicrosoftAppPassword="" 
+export UseTableStorageForConversationState=""
+
+export BotDevAppInsightsKey=""
+export BotDevAppInsightsName=""
+export BotDevAppInsightsAppId=""
+
+export LuisAPIKey=""
+export LuisAppId=""
+export LuisAPIHostName=""
+export BotOpenIdMetadata=""
+
+# export NODE_ENV="development"
+
+########################### Custom Keys taken from respective sources
+
+export BingSearchAPIKey=""
+export TextAnalyticsAPIKey=""
+export BingEntitySearchAPIKey=""
+export AcademicKnowledgeAPIKey=""
+export BingSearchAPIKey2=""
+export LinguisticsAPIKey=""
+export OXFORDAPPID="";
+export OXFORDAPPKEY="";
+
+########################### form url
+
+export FeedbackFormURL=""
